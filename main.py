c11954b0405ce819557cd8a220387614b19fafe2
dangthanhtri <48786737+dangthanhtri@users.noreply.github.com>
2023-08-06 13:13:25 +0700

    Update to work with tank wheel and extensions.
    Press B more to change extension.

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ rover.show_rgb_led(0, hex_to_rgb('#ff0000'))
 display.set_all('#ff0000')
 music.play(music.POWER_UP, wait=False)
 
-print('Rover started and ready')
-
 ROBOT_MODE_DO_NOTHING = const(31)
 ROBOT_MODE_AVOID_OBS = const(32)
 ROBOT_MODE_FOLLOW = const(33)
@@ -34,6 +32,20 @@ current_speed = 100
 key = KEY_NONE
 ble_connected = False
 
+robot_types_name = ['Basic - Gripper', 'Basic - Ball Launcher',
+                    'Tank - Gripper', 'Tank - Ball Launcher']
+robot_types = [[0, 0], [0, 1], [1, 0], [1, 1]]
+
+if device_config.get('robot_type', False):
+    print('Finish loading robot type')
+    robot_type = device_config['robot_type']
+else:
+    robot_type = [0, 0]
+
+__robot_type = robot_types.index(robot_type)
+
+print('Rover', robot_types_name[__robot_type], 'started and ready')
+
 
 def on_button_a_pressed():
     global mode, mode_changed
@@ -53,6 +65,48 @@ def on_button_a_pressed():
 
 
 button_a.on_pressed = on_button_a_pressed
+
+
+def on_button_b_pressed():
+    global mode, mode_changed, __robot_type, robot_type, robot_types
+    if mode == ROBOT_MODE_DO_NOTHING:
+        music.play(['G3:1'], wait=True)
+        press_begin = time.ticks_ms()
+        press_duration = 0
+
+        while button_b.is_pressed():
+            press_end = time.ticks_ms()
+            press_duration = time.ticks_diff(press_end, press_begin)
+
+            if press_duration > 2000:
+                print('robot type processing...')
+                __robot_type = (__robot_type if isinstance(
+                    __robot_type, (int, float)) else 0) + 1
+                if __robot_type > (len(robot_types) - 1):
+                    __robot_type = 0
+                display.scroll(__robot_type + 1)
+                time.sleep_ms(1000)
+
+        time.sleep_ms(2000)
+
+        if __robot_type != robot_type:
+
+            device_config['robot_type'] = robot_types[__robot_type]
+            save_config()
+
+            for count in range(3):
+                display.set_all('#ffa500')
+                time.sleep_ms(200)
+                display.set_all('#000000')
+                time.sleep_ms(200)
+
+            reset()
+        else:
+            display.set_all('#000000')
+            print('robot type exit without change.')
+
+
+button_b.on_pressed = on_button_b_pressed
 
 
 def ir_callback(cmd, addr, ext):
@@ -127,23 +181,47 @@ ble.on_disconnected(on_ble_disconnected_callback)
 
 
 def on_ble_message_string_receive_callback(chu_E1_BB_97i):
-    global mode, mode_changed
+    global mode, mode_changed, current_speed, robot_types, robot_type
     if chu_E1_BB_97i == ('!B516'):
-        rover.forward(50)
+        if robot_type[0] == 0:
+            rover.forward(50)
+        else:
+            rover.forward(100)
     elif chu_E1_BB_97i == ('!B615'):
-        rover.backward(50)
+        if robot_type[0] == 0:
+            rover.backward(50)
+        else:
+            rover.backward(100)
     elif chu_E1_BB_97i == ('!B714'):
-        rover.turn_left(50)
+        if robot_type[0] == 0:
+            rover.turn_left(50)
+        else:
+            rover.turn_left(90)
     elif chu_E1_BB_97i == ('!B814'):
-        rover.turn_right(50)
+        if robot_type[0] == 0:
+            rover.turn_right(50)
+        else:
+            rover.turn_right(90)
     elif chu_E1_BB_97i == ('!B11:'):  # A
-        rover.servo_write(1, 0)
+        if robot_type[1] == 0:
+            rover.servo_write(1, 0)
+        else:
+            launch_ball()
     elif chu_E1_BB_97i == ('!B219'):  # B
-        rover.servo_write(2, 90)
+        if robot_type[1] == 0:
+            rover.servo_write(2, 90)
+        else:
+            shoot_ball()
     elif chu_E1_BB_97i == ('!B318'):  # C
-        rover.servo_write(2, 0)
+        if robot_type[1] == 0:
+            rover.servo_write(2, 0)
+        else:
+            decr_speed()
     elif chu_E1_BB_97i == ('!B417'):  # D
-        rover.servo_write(1, 90)
+        if robot_type[1] == 0:
+            rover.servo_write(1, 90)
+        else:
+            incr_speed()
     else:
         rover.stop()
 
@@ -176,8 +254,44 @@ def on_ble_message_name_value_receive_callback(name, value):
 
 ble.on_receive_msg("name_value", on_ble_message_name_value_receive_callback)
 
+
+def incr_speed():
+    global current_speed
+    current_speed = (current_speed if isinstance(
+        current_speed, (int, float)) else 0) + 5
+    if current_speed >= 100:
+        current_speed = 100
+
+
+def decr_speed():
+    global current_speed
+    current_speed = (current_speed if isinstance(
+        current_speed, (int, float)) else 0) + -5
+    if current_speed < 20:
+        current_speed = 20
+
+
+def launch_ball():
+    global current_speed
+    rover.servo_write(1, 180)
+    time.sleep_ms(1000)
+    rover.servo_write(2, 180)
+    time.sleep_ms(1000)
+    rover.servo_write(1, 90)
+    time.sleep_ms(1000)
+    rover.servo_write(2, 20)
+    time.sleep_ms(1000)
+
+
+def shoot_ball():
+    global current_speed
+    rover.servo_write(1, 180)
+    time.sleep_ms(1000)
+
+
 try:
     while True:
+
         if mode_changed:
             if mode == ROBOT_MODE_DO_NOTHING:
                 rover.show_rgb_led(0, hex_to_rgb('#ff0000'))
@@ -222,39 +336,73 @@ try:
                 time.sleep_ms(100)
 
         elif mode == ROBOT_MODE_AVOID_OBS:
-            if rover.ultrasonic.distance_cm() < 15:
-                rover.backward(50, 0.5)
-                rover.turn_right(50, 0.25)
+            if robot_type[0] == 0:
+                if rover.ultrasonic.distance_cm() < 15:
+                    rover.backward(50, 0.5)
+                    rover.turn_right(50, 0.25)
+                else:
+                    rover.forward(50)
             else:
-                rover.forward(50)
+                if rover.ultrasonic.distance_cm() < 15:
+                    rover.backward(90, 1)
+                    rover.turn_right(90, 1)
+                else:
+                    rover.forward(90)
 
         elif mode == ROBOT_MODE_FOLLOW:
             obs_distance = rover.ultrasonic.distance_cm()
 
-            if obs_distance < 15:
-                rover.backward(50)
-            elif obs_distance < 30:
-                rover.stop()
-            elif obs_distance < 50:
-                rover.forward(50)
+            if robot_type[0] == 0:
+                if obs_distance < 15:
+                    rover.backward(50)
+                elif obs_distance < 30:
+                    rover.stop()
+                elif obs_distance < 50:
+                    rover.forward(50)
+                else:
+                    rover.stop()
             else:
-                rover.stop()
-            time.sleep_ms(50)
+                if obs_distance < 15:
+                    rover.backward(90)
+                elif obs_distance < 30:
+                    rover.stop()
+                elif obs_distance < 50:
+                    rover.forward(90)
+                else:
+                    rover.stop()
+            time.sleep_ms(20)
 
         elif mode == ROBOT_MODE_LINE_FINDER:
-            if rover.read_line_sensors() == (1, 0, 0, 0):
-                rover.turn_left(50)
-            elif rover.read_line_sensors() == (1, 1, 0, 0):
-                rover.turn_left(30)
-            elif rover.read_line_sensors() == (0, 0, 0, 1):
-                rover.turn_right(50)
-            elif rover.read_line_sensors() == (0, 0, 1, 1):
-                rover.turn_right(30)
-            elif rover.read_line_sensors() == (0, 0, 0, 0):
-                # while not ((rover.read_line_sensors(0)) or (rover.read_line_sensors(1)) or (rover.read_line_sensors(2)) or (rover.read_line_sensors(3))):
-                rover.backward(20)
+            if robot_type[0] == 0:
+                if rover.read_line_sensors() == (1, 0, 0, 0):
+                    rover.turn_left(50)
+                elif rover.read_line_sensors() == (1, 1, 0, 0):
+                    rover.turn_left(30)
+                elif rover.read_line_sensors() == (0, 0, 0, 1):
+                    rover.turn_right(50)
+                elif rover.read_line_sensors() == (0, 0, 1, 1):
+                    rover.turn_right(30)
+                elif rover.read_line_sensors() == (0, 0, 0, 0):
+                    # while not ((rover.read_line_sensors(0)) or (rover.read_line_sensors(1)) or (rover.read_line_sensors(2)) or (rover.read_line_sensors(3))):
+                    rover.backward(20)
+                else:
+                    rover.forward(25)
+
             else:
-                rover.forward(25)
+                if rover.read_line_sensors() == (1, 0, 0, 0):
+                    rover.turn_left(100)
+                elif rover.read_line_sensors() == (1, 1, 0, 0):
+                    rover.turn_left(90)
+                elif rover.read_line_sensors() == (0, 0, 0, 1):
+                    rover.turn_right(100)
+                elif rover.read_line_sensors() == (0, 0, 1, 1):
+                    rover.turn_right(90)
+                elif rover.read_line_sensors() == (0, 0, 0, 0):
+                    # while not ((rover.read_line_sensors(0)) or (rover.read_line_sensors(1)) or (rover.read_line_sensors(2)) or (rover.read_line_sensors(3))):
+                    rover.backward(80)
+                else:
+                    rover.forward(80)
+
 
 except KeyboardInterrupt:
     print('Rover program stopped')
@@ -266,5 +414,5 @@ finally:
     ble.on_receive_msg("string", None)
     ble.on_connected(None)
     ble.on_disconnected(None)
-    del mode, mode_changed, current_speed, ble_connected, key, on_ble_message_string_receive_callback, on_ble_connected_callback, on_ble_disconnected_callback, on_button_a_pressed
+    del mode, mode_changed, current_speed, ble_connected, key, on_ble_message_string_receive_callback, on_ble_connected_callback, on_ble_disconnected_callback, on_button_a_pressed, on_button_b_pressed, robot_type, robot_types
     gc.collect()
